b9d5a5c120b9fe65d57b3f9073aa9cf58cfc7056
dhartish21 <134483999+dhartish21@users.noreply.github.com>
2024-03-08 16:25:00 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+
+import { Icons } from 'react-toastify'
+function Footer() {
+    return (
+        <div>
+            <section className="info_section " >
+                <div className="container" >
+                    <h4>
+                        Get In Touch
+                    </h4>
+                    <div className="row">
+                        <div className="col-lg-10 mx-auto">
+                            <div className="info_items">
+                                <div className="row">
+                                    <div className="col-md-4">
+                                        <a href>
+                                            <div className="item ">
+                                                <div className="img-box ">
+                                                    <i className="fa fa-map-marker" aria-hidden="true" />
+                                                </div>
+                                                <p>
+                                                    Lorem Ipsum is simply dummy text
+                                                </p>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div className="col-md-4">
+                                        <a href>
+                                            <div className="item ">
+                                                <div className="img-box ">
+                                                    <i className="fa fa-phone" aria-hidden="true" />
+                                                </div>
+                                                <p>
+                                                    +02 1234567890
+                                                </p>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div className="col-md-4">
+                                        <a href>
+                                            <div className="item ">
+                                                <div className="img-box">
+                                                    <i className="fa fa-envelope" aria-hidden="true" />
+                                                </div>
+                                                <p>
+                                                    demo@gmail.com
+                                                </p>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                  
+               
+               
+                
+            </section>
+           
+            {/* end info_section */}
+            {/* footer section */}
+
+        
+</div>
+    )
+}
+
+export default Footer
