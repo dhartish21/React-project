6bdfd6b624a0c16a43147ffe5d66e10dc949f9a4
dhartish21 <134483999+dhartish21@users.noreply.github.com>
2024-03-08 16:27:03 +0530

    Add files via upload

---
+++ About.jsx
@@ -0,0 +1,171 @@
+import React from 'react'
+
+
+function About() {
+  return (
+  
+     <div>
+
+    <div>
+  <div classname="container">
+    <strong><span>Little</span> Fashion</strong>
+
+    
+        </div>
+
+  <section className="testimonial section-padding">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-9 mx-auto col-11">
+          <h2 className="text-center">Customer love, <br /> <span>Little</span> Fashion</h2>
+          <div className="slick-testimonial">
+            <div className="slick-testimonial-caption">
+              <p className="lead">Over three years in business, We’ve had the chance to work on a variety of projects, with companies Lorem ipsum dolor sit amet</p>
+              <div className="slick-testimonial-client d-flex align-items-center mt-4">
+                <img src="images/people/client2.jpeg" className="img-fluid custom-circle-image me-3" alt />
+                <span>George, <strong className="text-muted">Digital Art Fashion</strong></span>
+              </div>
+            </div>
+            <div className="slick-testimonial-caption">
+              <p className="lead">Over three years in business, We’ve had the chance to work on a variety of projects, with companies Lorem ipsum dolor sit amet</p>
+              <div className="slick-testimonial-client d-flex align-items-center mt-4">
+                <img src="images/people/beautiful-woman-face-portrait-brown-background.jpeg" className="img-fluid custom-circle-image me-3" alt />
+                <span>Sandar, <strong className="text-muted">Zoom Fashion Idea</strong></span>
+              </div>
+            </div>
+            <div className="slick-testimonial-caption">
+              <p className="lead">Over three years in business, We’ve had the chance to work on a variety of projects, with companies Lorem ipsum dolor sit amet</p>
+              <div className="slick-testimonial-client d-flex align-items-center mt-4">
+                <img src="images/people/portrait-british-woman.jpeg" className="img-fluid custom-circle-image me-3" alt />
+                <span>Marie, <strong className="text-muted">Art Fashion Design</strong></span>
+              </div>
+            </div>
+            <div className="slick-testimonial-caption">
+              <p className="lead">Over three years in business, We’ve had the chance to work on a variety of projects, with companies Lorem ipsum dolor sit amet</p>
+              <div className="slick-testimonial-client d-flex align-items-center mt-4">
+                <img src="images/people/woman-wearing-mask-face-closeup-covid-19-green-background.jpeg" className="img-fluid custom-circle-image me-3" alt />
+                <span>Catherine, <strong className="text-muted">Dress Fashion</strong></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <footer className="site-footer">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-3 col-10 me-auto mb-4">
+          <h4 className="text-white mb-3"><a href="index.html">Little</a> Fashion</h4>
+          <p className="copyright-text text-muted mt-lg-5 mb-4 mb-lg-0">Copyright © 2022 <strong>Little Fashion</strong></p>
+          <br />
+          <p className="copyright-text">Designed by <a href="https://www.tooplate.com/" target="_blank">Tooplate</a></p>
+        </div>
+      
+        <div className="col-lg-3 col-4">
+          <h5 className="text-white mb-3">Social</h5>
+          <ul className="social-icon">
+            <li><a href="#" className="social-icon-link bi-youtube" /></li>
+            <li><a href="#" className="social-icon-link bi-whatsapp" /></li>
+            <li><a href="#" className="social-icon-link bi-instagram" /></li>
+            <li><a href="#" className="social-icon-link bi-skype" /></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+  {/* TEAM MEMBER MODAL */}
+  <div className="modal fade" id="don" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div className="modal-dialog modal-dialog-centered modal-lg">
+      <div className="modal-content border-0">
+        <div className="modal-header flex-column">
+          <h3 className="modal-title" id="exampleModalLabel">Don Haruko</h3>
+          <h6 className="text-muted">Product, VP</h6>
+          <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+        </div>
+        <div className="modal-body">
+          <h5 className="mb-4">Over three years in business had the chance to work on variety of projects, with companies</h5>
+          <div className="row mb-4">
+            <div className="col-lg-6 col-12">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div className="col-lg-6 col-12">
+              <p>Incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse commodo viverra.</p>
+            </div>
+          </div>
+          <ul className="social-icon">
+            <li className="me-3"><strong>Where to find?</strong></li>
+            <li><a href="#" className="social-icon-link bi-youtube" /></li>
+            <li><a href="#" className="social-icon-link bi-whatsapp" /></li>
+            <li><a href="#" className="social-icon-link bi-instagram" /></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  {/* TEAM MEMBER MODAL */}
+  <div className="modal fade" id="kelly" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div className="modal-dialog modal-dialog-centered modal-lg">
+      <div className="modal-content border-0">
+        <div className="modal-header flex-column">
+          <h3 className="modal-title" id="exampleModalLabel">Kelly Lisa</h3>
+          <h6 className="text-muted">Global, Head of Marketing</h6>
+          <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+        </div>
+        <div className="modal-body">
+          <h5 className="mb-4">Over three years in business had the chance to work on variety of projects, with companies</h5>
+          <div className="row mb-4">
+            <div className="col-lg-6 col-12">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div className="col-lg-6 col-12">
+              <p>Incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse commodo viverra.</p>
+            </div>
+          </div>
+          <ul className="social-icon">
+            <li className="me-3"><strong>Where to find?</strong></li>
+            <li><a href="#" className="social-icon-link bi-facebook" /></li>
+            <li><a href="#" className="social-icon-link bi-instagram" /></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  {/* TEAM MEMBER MODAL */}
+  <div className="modal fade" id="marie" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div className="modal-dialog modal-dialog-centered modal-lg">
+      <div className="modal-content border-0">
+        <div className="modal-header flex-column">
+          <h3 className="modal-title" id="exampleModalLabel">Marie Sam</h3>
+          <h6 className="text-muted">Founder &amp; CEO</h6>
+          <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+        </div>
+        <div className="modal-body">
+          <h5 className="mb-4">Over three years in business had the chance to work on variety of projects, with companies</h5>
+          <div className="row mb-4">
+            <div className="col-lg-6 col-12">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div className="col-lg-6 col-12">
+              <p>Incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse commodo viverra.</p>
+            </div>
+          </div>
+          <ul className="social-icon">
+            <li className="me-3"><strong>Where to find?</strong></li>
+            <li><a href="#" className="social-icon-link bi-twitter" /></li>
+            <li><a href="#" className="social-icon-link bi-linkedin" /></li>
+            <li><a href="#" className="social-icon-link bi-envelope" /></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+    
+        </div>
+
+  )
+}
+
+export default About
